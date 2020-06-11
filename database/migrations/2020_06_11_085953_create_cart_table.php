f89a72fbf59af056249c6c46cd72a23a5bb0b641
Ionut Birlad <birladionut@yahoo.it>
2020-06-11 11:02:49 +0200

    create le migrations

---
+++ database/migrations/2020_06_11_085953_create_cart_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('advertising_id')->constrained();
+            $table->foreignId('apartament_id')->constrained();
+            $table->string('transaction');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart');
+    }
+}
